a904b76adb1fbf0db0c76bc53f01261d3ec6bff4
Ege Sucu <ege.sucu@pinsoft.ist>
2022-03-11 14:50:33 +0300

    Initial Commit

---
+++ PSKit/PSKit.h
@@ -0,0 +1,18 @@
+//
+//  PSKit.h
+//  PSKit
+//
+//  Created by Ege Sucu on 11.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PSKit.
+FOUNDATION_EXPORT double PSKitVersionNumber;
+
+//! Project version string for PSKit.
+FOUNDATION_EXPORT const unsigned char PSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSKit/PublicHeader.h>
+
+
